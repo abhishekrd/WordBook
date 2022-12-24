aae5a271bdd886a55f829aed3533ed66002dc21a
Abhishek Dhanke <abhishekrdhanke636@gmail.com>
2022-12-25 02:09:55 +0530

    refactor : remove unwanted logs

--- src/App.js
+++ src/App.js
@@ -21,8 +21,6 @@ function App() {
     }
   }
 
-  console.log(means.data);
-
   useEffect(() => {
     getWords()
   },[word])
--- src/components/Meanings.js
+++ src/components/Meanings.js
@@ -3,7 +3,6 @@ import React from 'react'
 
 const Meanings = ({means,word,defs}) => {
  
-console.log(means.data);
   return (
     <Container>
     <div>
